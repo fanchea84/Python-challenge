9a7441ebba6d5f083f532fa1d5fb49081bc40b73
fanchea84 <Adam.Fancher@nike.com>
2020-10-11 17:50:00 -0700

    add new file

--- PyBank/FullCodeWithTroubleshootingPrints.py
+++ PyBank/FullCodeWithTroubleshootingPrints.py
@@ -25,17 +25,27 @@ with open(budgetdatapath) as csvfile:
         month_count = month_count + 1
         total_profit += int(row[1])
         if month_count == 1: #for first month: +1 to running month count, and set profit total aggregate equal to current month profit 
+            print(f"month count ",month_count)
             new_month = int(row[1])
+            print(f"new month ",new_month)
             profit_change_current_month = new_month - old_month
             profit_total_aggregate = new_month = int(row[1])
+            print(f"profit total aggregate", profit_total_aggregate)
+            print("------------------------------")
         elif month_count != 1: #for months 2+, : +1 to running month count, and do some other stuff
+            print(f"month count ",month_count)
             old_month = new_month
+            print(f"old month", old_month)
             new_month = int(row[1])
+            print(f"new month ",new_month)
             if month_count == 2: # ignore month 2 for average profit change
                 profit_change_prior_month = 0
+                print(f"profit change prior month ",profit_change_prior_month)
             else: 
                 profit_change_prior_month = profit_change_current_month
+                print(f"profit change prior month ",profit_change_prior_month)
             profit_change_current_month = new_month - old_month
+            print(f"profit change current month", profit_change_current_month)
             if profit_change_current_month > profit_change_highest: # find month with largest month-over-month profit increase
                 profit_change_highest = profit_change_current_month
                 profit_change_highest_month = str(row[0])
@@ -44,9 +54,14 @@ with open(budgetdatapath) as csvfile:
                 profit_change_lowest = profit_change_current_month
                 profit_change_lowest_month = str(row[0])
             else: profit_change_lowest = profit_change_lowest
+            print(f"profit change highest", profit_change_highest_month, profit_change_highest)
             profit_change_running_total = profit_change_running_total + profit_change_current_month
+            print(f"profit change running total",profit_change_lowest_month, profit_change_running_total)
             profit_change_average = profit_change_running_total / (month_count - 1)
+            print(f"profit change average ",profit_change_average)
             profit_total_aggregate = profit_total_aggregate + new_month
+            print(f"profit total aggregate", profit_total_aggregate)
+            print("------------------------------")
         else:
             print("error")
 # Print table summary
--- PyBank/test.py
+++
@@ -1,9 +0,0 @@
-# Import OS and CSV modules
-import os
-import csv
-# read in budgetdata.csv
-budgetdatapath = "PyBank/Resources/BudgetData.csv"
-with open(budgetdatapath) as csvfile:
-    budgetdata = csv.reader(csvfile, delimiter=",")
-    for row in budgetdata:
-        print(row[1])
